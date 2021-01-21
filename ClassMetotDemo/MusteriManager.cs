3a2700a9d03897a0ed7bd822619b493e19dc15ea
MeldaKose <59098685+MeldaKose@users.noreply.github.com>
2021-01-22 02:27:39 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + "  " + musteri.Ad + "  " + "Eklendi.");
+        }
+        public void Listele(Musteri[] musteri)
+        {
+            Console.WriteLine("----------------- Müşteri Listesi -----------------");
+            foreach (Musteri item in musteri)
+            {
+                Console.WriteLine(item.Id + "  " + item.Ad + "  " + item.Soyad);
+
+            }
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + "  " + musteri.Ad + "  " + "Silindi.");
+        }
+    }
+}
